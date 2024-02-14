d5b4b36b1d2ecaaf11b851c4192f99b2988a9764
KostaV0108 <122493283+KostaV0108@users.noreply.github.com>
2024-02-14 15:46:35 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,11 @@
 def wordCount(t):
+word_dict = {}
+    with open(t, 'r') as file:
+        for line_num, line in enumerate(file, start=1):
+            words = line.strip().split()
+            for word in words:
+                if word not in word_dict:
+                    word_dict[word] = [line_num]
+                else:
+                    word_dict[word].append(line_num)
+    return word_dict
